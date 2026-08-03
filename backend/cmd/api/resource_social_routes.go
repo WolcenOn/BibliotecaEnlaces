b1c32baab0744dc1ffc34bf0db17b4596d128dd6
WolcenOn <fhaldon@gmail.com>
2026-08-03 12:55:39 +0100

    Conectar rutas de administración de miembros

--- backend/cmd/api/resource_social_routes.go
+++ backend/cmd/api/resource_social_routes.go
@@ -9,4 +9,7 @@ func (a *api) registerResourceSocialRoutes(mux *http.ServeMux) {
 	mux.Handle("POST /api/v1/resources/{resourceID}/comments", a.requireAuth(http.HandlerFunc(a.resourceComments)))
 	mux.Handle("PATCH /api/v1/resources/{resourceID}", a.requireAuth(http.HandlerFunc(a.updateResource)))
 	mux.Handle("DELETE /api/v1/resources/{resourceID}", a.requireAuth(http.HandlerFunc(a.deleteResource)))
+	mux.Handle("GET /api/v1/groups/{groupID}/managed-members", a.requireAuth(http.HandlerFunc(a.managedMembers)))
+	mux.Handle("PATCH /api/v1/groups/{groupID}/managed-members/{userID}", a.requireAuth(http.HandlerFunc(a.updateManagedMember)))
+	mux.Handle("DELETE /api/v1/groups/{groupID}/managed-members/{userID}", a.requireAuth(http.HandlerFunc(a.deleteManagedMember)))
 }
