d547de098e96ed0aac45cf4d33201d911904175f
WolcenOn <fhaldon@gmail.com>
2026-08-03 11:49:54 +0100

    Evitar ruta duplicada al iniciar la API

--- backend/cmd/api/resource_social_routes.go
+++ backend/cmd/api/resource_social_routes.go
@@ -4,7 +4,6 @@ import "net/http"
 
 func (a *api) registerResourceSocialRoutes(mux *http.ServeMux) {
 	mux.Handle("GET /api/v1/groups/{groupID}/resource-dashboard", a.requireAuth(http.HandlerFunc(a.resourceDashboard)))
-	mux.Handle("GET /api/v1/groups/{groupID}/members", a.requireAuth(http.HandlerFunc(a.groupMembers)))
 	mux.Handle("PUT /api/v1/resources/{resourceID}/rating", a.requireAuth(http.HandlerFunc(a.rateResource)))
 	mux.Handle("GET /api/v1/resources/{resourceID}/comments", a.requireAuth(http.HandlerFunc(a.resourceComments)))
 	mux.Handle("POST /api/v1/resources/{resourceID}/comments", a.requireAuth(http.HandlerFunc(a.resourceComments)))
