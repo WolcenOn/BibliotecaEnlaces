03015bb6834f1baab3e4364d599eab215625466f
WolcenOn <fhaldon@gmail.com>
2026-08-03 11:35:22 +0100

    Definir rutas sociales de recursos

---
+++ backend/cmd/api/resource_social_routes.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func (a *api) registerResourceSocialRoutes(mux *http.ServeMux) {
+	mux.Handle("GET /api/v1/groups/{groupID}/resource-dashboard", a.requireAuth(http.HandlerFunc(a.resourceDashboard)))
+	mux.Handle("GET /api/v1/groups/{groupID}/members", a.requireAuth(http.HandlerFunc(a.groupMembers)))
+	mux.Handle("PUT /api/v1/resources/{resourceID}/rating", a.requireAuth(http.HandlerFunc(a.rateResource)))
+	mux.Handle("GET /api/v1/resources/{resourceID}/comments", a.requireAuth(http.HandlerFunc(a.resourceComments)))
+	mux.Handle("POST /api/v1/resources/{resourceID}/comments", a.requireAuth(http.HandlerFunc(a.resourceComments)))
+	mux.Handle("PATCH /api/v1/resources/{resourceID}", a.requireAuth(http.HandlerFunc(a.updateResource)))
+	mux.Handle("DELETE /api/v1/resources/{resourceID}", a.requireAuth(http.HandlerFunc(a.deleteResource)))
+}
